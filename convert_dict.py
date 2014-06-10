75caad50895e53921233a38439386cb87d97a0ad
Yaroslav Ganin <yaroslav.ganin@gmail.com>
2014-06-10 14:56:54 +0400

    NYU experiment added.

--- convert_dict.py
+++ convert_dict.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 
 import os
+import glob
 import struct
 import numpy as np
 import scipy.io
 
-dict_patches_path = '/home/yganin/Arbeit/Projects/NN/Segmentation/Lab/Reconstruction/samples_bsds500.mat'
+dict_patches_path = '/home/yganin/Arbeit/Projects/NN/Segmentation/Lab/Reconstruction/samples_nyu.mat'
 
 def load_samples(filename):
     if os.path.exists(filename):
@@ -22,12 +23,12 @@ def load_samples(filename):
     samples = []
 
     for i in xrange(num_files):
-    	s = scipy.io.loadmat('%s_%d.mat' % (filename, i))
+        s = scipy.io.loadmat('%s_%d.mat' % (filename, i))
         s = s['samples']
         
         samples += [s]
 
-    samples = n.vstack(tuple(samples))
+    samples = np.vstack(tuple(samples))
 
     return samples 
 
@@ -38,20 +39,21 @@ def process_samples():
     print(num_samples)
 
     with open('dict_processed_patches.bin', 'wb') as f:
-    	f.write(struct.pack('I', num_samples))
+        f.write(struct.pack('I', num_samples))
 
-    	for i in xrange(num_samples):
-	    	sample = np.single(samples[i][0])
+        for i in xrange(num_samples):
+            sample = np.single(samples[i][0])
 
-	        if sample.max() > 1.0:
-	            sample = sample / 255.0
+            if sample.max() > 1.0:
+                sample = sample / 255.0
+                print(sample.max())
 
-	        sample -= np.array([0.43420864, 0.44378472, 0.37162688], dtype=np.single).reshape((1, 1, sample.shape[2]))
+            sample -= np.array([0.4911259, 0.42236657, 0.4007874, 0.21501289], dtype=np.single).reshape((1, 1, sample.shape[2]))
 
-	        if i == 0:
-	            print(sample.flags)
-	            print(sample.shape)
+            if i == 0:
+                print(sample.flags)
+                print(sample.shape)
 
-	        sample.transpose((2, 0, 1)).flatten().tofile(f)
+            sample.transpose((2, 0, 1)).flatten().tofile(f)
 
 process_samples()
---
+++ uber_script.sh
@@ -0,0 +1,6 @@
+python \
+./convert_weights.py \
+-p /home/yganin/Arbeit/Projects/NN/mynet/sandbox/checkpoints/pca+more_maps+add_fc+drop_3-nyu/ConvNet__2014-06-09_17.34.45/75000.1
+
+python \
+./run_test.py -t ./nyu_1
